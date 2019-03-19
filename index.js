b2953224bf9b5df6367c78059798cceb6a388d9c
aleph <alephspb@gmail.com>
2019-03-18 20:13:24 -0400

    bug fixing

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
-
+var Promise = require('promise');
 var app = require('./lib/server');
 var base = require('./lib/base');
 var data = require('./lib/data');
 
-base(app);
-data(app);
+Promise.all([base(app), data(app)]).then(function () {
+    resolve(app);
+}).catch(function (reason) {
+    reject(reason);
+});
 
 module.exports = app;
